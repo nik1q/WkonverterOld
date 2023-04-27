f99907eea986916dcc0792157003a7a1f4ad3cb7
Nik-MnemoNik <msanik@mail.ru>
2023-04-27 22:04:29 +0200

    add API

--- app/src/main/java/de/nik1q/wkonverter/CurrencyApiService.kt
+++ app/src/main/java/de/nik1q/wkonverter/CurrencyApiService.kt
@@ -1,23 +1,42 @@
 package de.nik1q.wkonverter
 
-import retrofit2.Call
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
+import com.google.gson.annotations.SerializedName
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-
-
-interface ExchangeRatesApiService {
-    @GET("/exchange-rates")
-    fun getExchangeRates(
+interface CurrencyApiService {
+    @GET("v1/live")
+    suspend fun getExchangeRates(
         @Query("api_key") apiKey: String,
-        @Query("base_currency") baseCurrency: String,
-        @Query("target_currencies") targetCurrencies: String
-    ): Call<Map<String, Map<String, Double>>>
+        @Query("base") base: String,
+        @Query("target") target: String
+    ): ExchangeRatesResponse
 }
 
-class CurrencyApiService {
+data class ExchangeRatesResponse(
+    @SerializedName("success")
+    val success: Boolean,
+    @SerializedName("timestamp")
+    val timestamp: Long,
+    @SerializedName("base")
+    val base: String,
+    @SerializedName("rates")
+    val rates: Map<String, Double>
+)
 
+object RetrofitInstance {
+    private const val BASE_URL = "https://exchange-rates.abstractapi.com/"
+
+    fun getRetrofitInstance(): Retrofit {
+        val gson = GsonBuilder().create()
+
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+    }
 }
+
--- app/src/main/java/de/nik1q/wkonverter/MainActivity.kt
+++ app/src/main/java/de/nik1q/wkonverter/MainActivity.kt
@@ -1,25 +1,58 @@
 package de.nik1q.wkonverter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.HttpException
+import java.io.IOException
 import de.nik1q.wkonverter.databinding.ActivityMainBinding
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.FileInputStream
+import java.util.Properties
+
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var currencyApiService: CurrencyApiService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://app.abstractapi.com/api")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+        currencyApiService = RetrofitInstance.getRetrofitInstance().create(CurrencyApiService::class.java)
 
-        val apiService = retrofit.create(ExchangeRatesApiService::class.java)
+        // Api
+        val apikey: String
+        val props = Properties()
+        try {
+            props.load(FileInputStream("local.properties"))
+            apikey = props.getProperty("API_KEY")
+        } catch (e: Exception) {
+            throw RuntimeException("API key not found", e)
+        }
+
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val response = currencyApiService.getExchangeRates(
+                    apiKey = "API_KEY",
+                    base = "USD",
+                    target = "EUR,CAD"
+                )
+
+                withContext(Dispatchers.Main) {
+                    // date
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            } catch (e: HttpException) {
+                e.printStackTrace()
+            }
+        }
     }
 }
 
